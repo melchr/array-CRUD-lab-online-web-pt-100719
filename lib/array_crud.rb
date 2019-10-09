e7bf1ee122311a0ec87eb0d338b510c712ae566e
Melissa Christman <melchrstmn@gmail.com>
2019-10-09 00:39:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array=["dogs", "fish", "cats", "bears"]
+  array = ["dogs", "fish", "cats", "bears"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array << "deer"
+  array.push("deer")
 end
 
 def add_element_to_start_of_array(array, element)
